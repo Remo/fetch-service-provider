c2a9ac45f6e13945692a1d6535989704a3c498ba
Remo <remo.laubacher@gmail.com>
2013-11-28 16:19:59 +0100

    basic structure for service provider added

---
+++ src/Tedivm/Fetch/FetchServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * (c) Remo Laubacher <remo.laubacher@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Silex\Provider;
+
+use Silex\Application,
+    Silex\ServiceProviderInterface;
+
+/**
+ * Tedivm/Fetch Service Provider for silex.
+ *
+ * @author Remo Laubacher <remo.laubacher@gmail.com>
+ */
+class SwiftmailerServiceProvider implements ServiceProviderInterface {
+
+    public function register(Application $app) {
+        
+    }
+
+    public function boot(Application $app) {
+        
+    }
+
+}
